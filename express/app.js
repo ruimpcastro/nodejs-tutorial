b7902df2cc250295fcd9384a5d4452d44e935464
Rui Castro <rui_castro@live.com.pt>
2022-04-23 21:32:15 +0000

    Finished 05-all-static

--- express/app.js
+++ express/app.js
@@ -4,9 +4,11 @@ const app = express();
 
 app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
+// app.get("/", (req, res) => {
+// res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding to static assets
+// SSR
+// });
 
 app.all("*", (req, res) => {
   res.status(404).send("<h1>404 page not found</h1>");
