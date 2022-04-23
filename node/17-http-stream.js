797b2449f99118e49c3d2ec3f6869257d9e83ffa
Rui Castro <rui_castro@live.com.pt>
2022-04-23 15:45:53 +0000

    Add express tutorial

--- node/17-http-stream.js
+++ node/17-http-stream.js
@@ -6,7 +6,7 @@ http
     // const text = fs.readFileSync('./content/big.txt', 'utf8')
     // res.end(text)
     const fileStream = fs.createReadStream("./content/big.txt", "utf8");
-    fileStream.on("open", () => {
+    fileStream.on("open", () => {  
       fileStream.pipe(res);
     });
     fileStream.on("error", (err) => {
