e99ac41606352b2c376339b83d75abfec10ce324
Rui Castro <rui_castro@live.com.pt>
2022-04-25 17:37:52 +0000

    Finished 06-basic-json

--- express/app.js
+++ express/app.js
@@ -1,19 +1,36 @@
 const express = require("express");
-const path = require("path");
+const http = require("http");
 const app = express();
+const { products } = require("./data");
 
-app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.send("<h1>Home page</h1><a href='/api/products'>Products</a>");
+});
 
-// app.get("/", (req, res) => {
-// res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// adding to static assets
-// SSR
-// });
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>404 page not found</h1>");
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product not found");
+  }
+  return res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  const { productID, reviewID } = req.params;
+  console.log(req.params);
 });
 
 app.listen(3000, () => {
-  console.log("server is listening on port 3000...");
+  console.log("Server is running on port 3000");
 });
