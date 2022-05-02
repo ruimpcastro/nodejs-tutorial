d13d769a086295cb2f97a20b8e89f0e389bd3c3e
Rui Castro <rui_castro@live.com.pt>
2022-05-02 17:18:10 +0000

    Finished lesson 10-middleware-options.js

--- express/app.js
+++ express/app.js
@@ -1,55 +1,29 @@
 const express = require("express");
-const http = require("http");
 const app = express();
-const { products } = require("./data");
+const morgan = require("morgan");
+const logger = require("./logger");
+const authorize = require("./authorize");
+//req => middleware => res
+
+// app.use([logger, authorize]);
+// app.use([express.static("./public")]);
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Home page</h1><a href='/api/products'>Products</a>");
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
 app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
+  res.send("Products");
 });
-
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product not found");
-  }
-  return res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello world");
-});
-
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send("no products matched your search");
-    res.status(200).json({ sucess: true, data: [] });
-  }
-  return res.status(200).json(sortedProducts);
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is listening on port 3000...");
 });
